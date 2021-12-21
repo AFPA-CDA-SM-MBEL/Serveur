1628bd767f6362d0238a15551abfdf55063c1c1e
Gilles HUBERT <gilles.hubert97@gmail.com>
2021-12-21 15:08:43 +0100

    Création des donées en lecture.

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ app.get("/", (req, res) => {
     res.send("Bonjour, le serveur écoute bien !")
 })
 
+app.get("/get", (req, res) => {
+    let data = require("./data.json")
+    res.send(data)
+})
+
 app.listen(3000, function () {
     console.log("Application d'exemple écoutant sur le port 3000 !")
 })
