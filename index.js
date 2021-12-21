f9cf25d75eb8839fdd6b8e946a9560d38fd512fa
Gilles HUBERT <gilles.hubert97@gmail.com>
2021-12-21 16:29:21 +0100

    Ajout de la lecture en BDD

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-let express = require('express')
+let express = require("express")
 let app = express()
+let { Sequelize } = require("sequelize");
+let mySQLidentifiants = require("./bdd/identifiants.json")
 
 app.get("/", (req, res) => {
     res.send("Bonjour, le serveur écoute bien !")
@@ -8,6 +10,26 @@ app.get("/", (req, res) => {
 app.get("/get", (req, res) => {
     let data = require("./data.json")
     res.send(data)
+})
+
+app.get("/getbdd", (req, res) => {
+    let sequelize = new Sequelize(
+        mySQLidentifiants["bdd"],
+        mySQLidentifiants["user"],
+        mySQLidentifiants["psw"],
+        {
+            dialect: "mysql",
+            host: "localhost"
+        }
+    )
+    try{
+        sequelize.authenticate()
+        sequelize.query("SELECT * FROM products").then(([results, metadata]) => {
+            res.send(results)
+        })
+    }catch(error){
+        res.send("Impossible de se connecter")
+    }
 })
 
 app.listen(3000, function () {
